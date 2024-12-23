d75ecd88c668dc61e157e263d13a0e39dd7dcbb7
visveshkhanna <visveshkhanna@gmail.com>
2024-12-23 15:24:27 +0530

    Initial Commit
    NextJS@14
    Shadcn + Dark Mode
    Acertenity Landing page.

---
+++ src/lib/provider.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@/components/ui/theme-provider";
+
+export default function MainProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </ThemeProvider>
+  );
+}
